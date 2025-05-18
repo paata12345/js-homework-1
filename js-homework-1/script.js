330a0b00d4a0bdca62437400bb48d0bebd167511
paata12345 <p.batsikadze@gmail.com>
2025-05-18 17:24:49 +0400

    js-homework-1

---
+++ js-homework-1/script.js
@@ -0,0 +1,25 @@
+// პაატა ბაციკაძე 18/05/25
+
+//შევქმენით ცვლადები და მივანიჭეთ მნიშვნელობა
+let firstName = "პაატა";
+let lastName = "ბაციკაძე";
+let email = "p.batsikadze@gmail.com";
+let phoneNumber = "599495646";
+let age = 24;
+let hobby = "cars";
+let isStudent = true;
+
+//გამოვიტანეთ კონსოლში
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Email: " + email);
+console.log("Phone Number: " + phoneNumber);
+console.log("Age: " + age);
+console.log("Hobby: " + hobby);
+console.log("Is Student: " + isStudent);
+//შევცვალეთ ჰობი და ასაკი
+hobby = "coding";
+age = age + 1;
+//გამოვიტანეთ შეცვლილი მნიშვნელობა
+console.log("Age: " + age);
+console.log("Hobby: " + hobby);
